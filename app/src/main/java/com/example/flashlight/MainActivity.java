724439fb16e1ff9202dd52424d0b25b2de04a572
icegps_cky <chenkaiyang@icegps.com.cn>
2021-07-14 18:10:10 +0800

    add

--- app/src/main/java/com/example/flashlight/MainActivity.java
+++ app/src/main/java/com/example/flashlight/MainActivity.java
@@ -14,7 +14,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.hardware.Camera;
-import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraManager;
 import android.net.Uri;
@@ -220,7 +219,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
 
             }
-        } catch (CameraAccessException exception) {
+        } catch (Exception exception) {
             exception.printStackTrace();
         }
 
